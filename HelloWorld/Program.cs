225d83bb24cbc92049db8b8965cdf0a24f90e811
marinakamardina <kamardina.mari@gmail.com>
2022-04-06 13:40:33 +0300

    add files

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's u first name?");
+            string userFirstName = Console.ReadLine(); //string - тип ячейки памяти для сохранения текста
+            Console.WriteLine("What's u last name?");
+            string userLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + userFirstName + " " + userLastName + "!");
+
+        }
+    }
+}
---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's u first name?");
+            string userFirstName = Console.ReadLine();
+            Console.WriteLine("What's u last name?");
+            string userLastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userFirstName} {userLastName}!");//string interpolation - $ означает, что используем данные из переменных
+                                                                         //userfirstname and userlastname
+        }
+    }
+}
